e7cc8c0550933e11b04fbf681556c42dc45f525b
Avital-Ha <avital.hamburg@gmail.com>
2024-09-18 13:29:03 +0300

    git commit "first component"

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="header">
+            <div className="logo-title">Your Logo</div>
+            <div className="small-title">React Course - Project 3</div>
+        </div>
+    );
+}
