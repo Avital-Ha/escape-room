9beb19f1793e84717917540bb13211e4d36b7364
Avital-Ha <avital.hamburg@gmail.com>
2024-09-18 14:24:39 +0300

    git commit-m""

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import image from '../assets/abandoned-morgue-psychiatric-hospital.jpg'; // Adjust the path as needed
+
+import './Home.css';
+
 import { Link } from 'react-router-dom'; // הוסף את הייבוא של Link
 
 function Home() {
   return (
-    <div>
+    <div className='home' style={{ backgroundImage: `url(${image})` }}>
       <h2>Home Page</h2>
       <ul>
         <li>
